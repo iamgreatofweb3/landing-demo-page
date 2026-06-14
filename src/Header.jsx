eb6e4831f9c0ae7d6b9ffb8ff5ac6f0848e6e5f4
iamgreatofweb3 <iamgreat045@gmail.com>
2026-06-14 14:19:59 +0100

    changes 3

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import { Moon,Sun} from "lucide-react"
 export default function Header () {
     return (
         <>
-        {/* <div className="main-container">
+        <div className="main-container">
 
         <header>
 
@@ -20,25 +20,9 @@ export default function Header () {
               </ul>    
             </div>
         </header>
-        </div> */}
-             <div className="header-container">
-          <div className="left-header">
-            <h3>ApexForge Technologies</h3>
-          </div>
-           {/* Space for the 2 diffrent flex divs */}
-            <div className="rightHeader">
-              <button ><Sun className="rightIcon"/></button>
-                  <ul className="dropDownContainer">
-                  <a href="#story"><li>Our Story</li></a>
-                  <a href="#products"><li>Products</li></a>
-                  <a href="#sustainability"><li>Sustainability</li></a>
-                  <a href="#partner"><li>Partner</li></a>
-                  <a href="#contact"><li>Contact</li></a>
-                <button className="button"><a href="#partner">Partner With Us</a></button>
-                </ul>
-            </div>
         </div>
-            
+           
+                
             
         </>
     )
